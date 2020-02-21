fd67e52d92423e663293b5d0ec6625e4c24a0f5f
Khmel <m.khmelevtsova@student.csn.khai.edu>
2020-02-21 17:32:25 +0200

    Creation of the lab1

---
+++ Sp1/Sp1.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+
+int main()
+{
+	
+}
+
+void ShowError()
+{
+	LPVOID lpMsgBuf;
+	DWORD dw = GetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, dw, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR) & lpMsgBuf, 0, NULL);
+	printf("%s", lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
