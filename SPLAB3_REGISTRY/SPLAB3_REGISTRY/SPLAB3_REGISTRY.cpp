1b2c3955218487226d855203b8fb7a369815e66b
mkhmelevtsova <m.khmelevtsova@student.csn.khai.edu>
2020-04-27 18:21:35 +0300

    Fixed some issues and minor flaws

--- SPLAB3_REGISTRY/SPLAB3_REGISTRY/SPLAB3_REGISTRY.cpp
+++ SPLAB3_REGISTRY/SPLAB3_REGISTRY/SPLAB3_REGISTRY.cpp
@@ -1,42 +1,43 @@
 #include "pch.h"
 
-INT PrintSubkeysList();
-INT PrintKeyParametres();
-INT SearchString();
+void PrintSubkeysList();
+void PrintKeyParametres();
+void SearchString();
 void KeyAnalyser(HKEY initialSection, TCHAR keyPath[], LPTSTR searchValue);
-INT SaveKey();
+void SaveKey();
 
 int main(int argc, LPTSTR argv[])
 {
     INT option;
     while (true) {
-        _tprintf(L"\nSelect option:\n1. Print subkeys list.\n2. Print key's parameters with values and types.\n3. Search string in registry.\n4. Export key in file.\n5. Exit.\nOption: ");
+        _tprintf(L"\nSelect option:\n1. Print subkeys list.\n2. Print key's parameters with values and types.\
+                    \n3. Search string in registry.\n4. Export key in file.\n5. Exit.\nOption: ");
         _tscanf(L"%i", &option);
         switch (option)
         {
             case 1:
             {
-                INT result = PrintSubkeysList();
+                PrintSubkeysList();
                 break;
             }
             case 2:
             {
-                INT result = PrintKeyParametres();
+                PrintKeyParametres();
                 break;
             }
             case 3:
             {
-                INT result = SearchString();
+                SearchString();
                 break;
             }
             case 4:
             {
-                INT result = SaveKey();
+                SaveKey();
                 break;
             }
             case 5:
             {
-                return 0;
+                return EXIT_SUCCESS;
                 break;
             }
             default:
@@ -46,10 +47,10 @@ int main(int argc, LPTSTR argv[])
             }
         }
     }
-	return 0;
+	return EXIT_SUCCESS;
 }
 
-INT PrintSubkeysList()
+void PrintSubkeysList()
 {
     LPTSTR keyName;
     HKEY    hKey = 0;
@@ -59,40 +60,42 @@ INT PrintSubkeysList()
     LPTSTR subkeyName = new TCHAR[256];
     LPTSTR subKey = new TCHAR[256];
     INT option;
-    _tprintf(L"\nSelect initial section:\n1. HKEY_CLASSES_ROOT\n2. HKEY_CURRENT_CONFIG\n3. HKEY_CURRENT_USER\n4. HKEY_LOCAL_MACHINE\n5. HKEY_USERS\nYour option: ");
+    _tprintf(L"\nSelect initial section:\n1. HKEY_CLASSES_ROOT\n2. HKEY_CURRENT_CONFIG\n\
+                3. HKEY_CURRENT_USER\n4. HKEY_LOCAL_MACHINE\n5. HKEY_USERS\nYour option: ");
     _tscanf(L"%i", &option);
     HKEY initialSection = option == 1 ? HKEY_CLASSES_ROOT : option == 2 ? HKEY_CURRENT_CONFIG : option == 3 ? HKEY_CURRENT_USER : option == 4 ? HKEY_LOCAL_MACHINE : option == 5 ? HKEY_USERS : NULL;
-    if (initialSection == NULL) return -1;
-    _tprintf(L"Input keys path (if you want to print initial section's subkeys list, enter 'd'): ");
-    _tscanf(L"%s", subKey);
-    if (lstrcmpW(subKey, L"d") == 0) subKey = (LPTSTR)L"";
-    LONG lRes = RegOpenKeyEx(initialSection, subKey, NULL, KEY_ENUMERATE_SUB_KEYS, &hKey);
-    if (lRes != ERROR_SUCCESS)
-    {
-        _tprintf(L"Error");
-    }
-    else
-    {
-        _tprintf(L"\n");
-        do
+    if (initialSection != NULL) {
+        _tprintf(L"Input keys path (if you want to print initial section's subkeys list, enter 'd'): ");
+        _tscanf(L"%s", subKey);
+        if (lstrcmpW(subKey, L"d") == 0) subKey = (LPTSTR)L"";
+        LONG lRes = RegOpenKeyEx(initialSection, subKey, NULL, KEY_ENUMERATE_SUB_KEYS, &hKey);
+        if (lRes != ERROR_SUCCESS)
         {
-            dwValue = 256;
-            subkeyResultCode = RegEnumKeyEx(hKey, dwIndex, subkeyName, &dwValue, NULL, NULL, NULL, NULL);//выводим папки
-            if (subkeyResultCode == ERROR_SUCCESS)
+            _tprintf(L"Error");
+        }
+        else
+        {
+            _tprintf(L"\n");
+            do
             {
-                _tprintf(L"%s\n", subkeyName);
-            }
-            else
-            {
-                keyName = (LPTSTR)L"Error!";
-            }
-            dwIndex++;
-        } while (subkeyResultCode != ERROR_NO_MORE_ITEMS);
+                dwValue = 256;
+                subkeyResultCode = RegEnumKeyEx(hKey, dwIndex, subkeyName, &dwValue, NULL, NULL, NULL, NULL);
+                if (subkeyResultCode == ERROR_SUCCESS)
+                {
+                    _tprintf(L"%s\n", subkeyName);
+                }
+                else
+                {
+                    keyName = (LPTSTR)L"Error!";
+                }
+                dwIndex++;
+            } while (subkeyResultCode != ERROR_NO_MORE_ITEMS);
+        }
+        RegCloseKey(hKey);
     }
-    RegCloseKey(hKey);
 }
 
-INT PrintKeyParametres()
+void PrintKeyParametres()
 {
     LPTSTR keyName;
     HKEY    hKey = 0;
@@ -105,57 +108,59 @@ INT PrintKeyParametres()
     DWORD parameterValueBufferSize = 256;
     LPTSTR subKey = new TCHAR[256];
     INT option;
-    _tprintf(L"\nSelect initial section:\n1. HKEY_CLASSES_ROOT\n2. HKEY_CURRENT_CONFIG\n3. HKEY_CURRENT_USER\n4. HKEY_LOCAL_MACHINE\n5. HKEY_USERS\nYour option: ");
+    _tprintf(L"\nSelect initial section:\n1. HKEY_CLASSES_ROOT\n2. HKEY_CURRENT_CONFIG\n\
+                3. HKEY_CURRENT_USER\n4. HKEY_LOCAL_MACHINE\n5. HKEY_USERS\nYour option: ");
     _tscanf(L"%i", &option);
     HKEY initialSection = option == 1 ? HKEY_CLASSES_ROOT : option == 2 ? HKEY_CURRENT_CONFIG : option == 3 ? HKEY_CURRENT_USER : option == 4 ? HKEY_LOCAL_MACHINE : option == 5 ? HKEY_USERS : NULL;
-    if (initialSection == NULL) return -1;
-    _tprintf(L"Input keys path (if you want to print initial section's parametres, enter 'd'): ");
-    _tscanf(L"%s", subKey);
-    if (lstrcmpW(subKey, L"d") == 0) subKey = (LPTSTR)L"";
-    LONG lRes = RegOpenKeyEx(initialSection, subKey, NULL, KEY_QUERY_VALUE, &hKey);
-    if (lRes != ERROR_SUCCESS)
-    {
-        _tprintf(L"Error");
-    }
-    else
-    {
-        _tprintf(L"\n");
-        _tprintf(L"| %-40s | %-24s | %s\n", L"Name", L"Type", L"Value");
-        do
+    if (initialSection != NULL) {
+        _tprintf(L"Input keys path (if you want to print initial section's parametres, enter 'd'): ");
+        _tscanf(L"%s", subKey);
+        if (lstrcmpW(subKey, L"d") == 0) subKey = (LPTSTR)L"";
+        LONG lRes = RegOpenKeyEx(initialSection, subKey, NULL, KEY_QUERY_VALUE, &hKey);
+        if (lRes != ERROR_SUCCESS)
         {
-            dwValue = 256;
-            subkeyResultCode = RegEnumValue(hKey, dwIndex, parameterName, &dwValue, NULL, &parameterType, parameterValue, &parameterValueBufferSize);
-            if (subkeyResultCode == ERROR_SUCCESS)
+            _tprintf(L"Error");
+        }
+        else
+        {
+            _tprintf(L"\n");
+            _tprintf(L"| %-40s | %-24s | %s\n", L"Name", L"Type", L"Value");
+            do
             {
-                LPTSTR parameterTypeN = new TCHAR[256];
-                switch (parameterType)
+                dwValue = 256;
+                subkeyResultCode = RegEnumValue(hKey, dwIndex, parameterName, &dwValue, NULL, &parameterType, parameterValue, &parameterValueBufferSize);
+                if (subkeyResultCode == ERROR_SUCCESS)
                 {
-                case 3: parameterTypeN = (LPTSTR)L"REG_BINARY"; break;
-                case 4: parameterTypeN = (LPTSTR)L"REG_DWORD"; break;
-                case 5: parameterTypeN = (LPTSTR)L"REG_REG_DWORD_BIG_ENDIAN"; break;
-                case 2: parameterTypeN = (LPTSTR)L"REG_EXPAND_SZ"; break;
-                case 6: parameterTypeN = (LPTSTR)L"REG_LINK"; break;
-                case 7: parameterTypeN = (LPTSTR)L"REG_MULTI_SZ"; break;
-                case 0: parameterTypeN = (LPTSTR)L"REG_NONE"; break;
-                case 8: parameterTypeN = (LPTSTR)L"REG_RESOURCE_LIST "; break;
-                case 1: parameterTypeN = (LPTSTR)L"REG_SZ"; break;
-                default: parameterTypeN = (LPTSTR)L"UNKOWN "; break;
+                    LPTSTR parameterTypeN = new TCHAR[256];
+                    switch (parameterType)
+                    {
+                    case REG_BINARY: parameterTypeN = (LPTSTR)L"REG_BINARY"; break;
+                    case REG_DWORD: parameterTypeN = (LPTSTR)L"REG_DWORD"; break;
+                    case REG_DWORD_BIG_ENDIAN: parameterTypeN = (LPTSTR)L"REG_DWORD_BIG_ENDIAN"; break;
+                    case REG_EXPAND_SZ: parameterTypeN = (LPTSTR)L"REG_EXPAND_SZ"; break;
+                    case REG_LINK: parameterTypeN = (LPTSTR)L"REG_LINK"; break;
+                    case REG_MULTI_SZ: parameterTypeN = (LPTSTR)L"REG_MULTI_SZ"; break;
+                    case REG_NONE: parameterTypeN = (LPTSTR)L"REG_NONE"; break;
+                    case REG_RESOURCE_LIST: parameterTypeN = (LPTSTR)L"REG_RESOURCE_LIST"; break;
+                    case REG_SZ: parameterTypeN = (LPTSTR)L"REG_SZ"; break;
+                    default: parameterTypeN = (LPTSTR)L"UNKOWN "; break;
+                    }
+                    _tprintf(L" %-40s   ", parameterName);
+                    _tprintf(L"%-24s   ", parameterTypeN);
+                    _tprintf(L"%s\n", (LPCTSTR)parameterValue);
                 }
-                _tprintf(L" %-40s   ", parameterName);
-                _tprintf(L"%-24s   ", parameterTypeN);
-                _tprintf(L"%s\n", (LPCTSTR)parameterValue);
-            }
-            else
-            {
-                keyName = (LPTSTR)L"Error!";
-            }
-            dwIndex++;
-        } while (subkeyResultCode != ERROR_NO_MORE_ITEMS);
+                else
+                {
+                    keyName = (LPTSTR)L"Error!";
+                }
+                dwIndex++;
+            } while (subkeyResultCode != ERROR_NO_MORE_ITEMS);
+        }
+        RegCloseKey(hKey);
     }
-    RegCloseKey(hKey);
 }
 
-INT SearchString() 
+void SearchString() 
 {
     LPTSTR searchValue = new TCHAR[256];
     HKEY    hKey = 0;
@@ -165,16 +170,18 @@ INT SearchString()
     LPTSTR subkeyName = new TCHAR[256];
     TCHAR subKey[1024];
     INT option;
-    _tprintf(L"\nSelect initial section:\n1. HKEY_CLASSES_ROOT\n2. HKEY_CURRENT_CONFIG\n3. HKEY_CURRENT_USER\n4. HKEY_LOCAL_MACHINE\n5. HKEY_USERS\nYour option: ");
+    _tprintf(L"\nSelect initial section:\n1. HKEY_CLASSES_ROOT\n2. HKEY_CURRENT_CONFIG\n\
+                3. HKEY_CURRENT_USER\n4. HKEY_LOCAL_MACHINE\n5. HKEY_USERS\nYour option: ");
     _tscanf(L"%i", &option);
     HKEY initialSection = option == 1 ? HKEY_CLASSES_ROOT : option == 2 ? HKEY_CURRENT_CONFIG : option == 3 ? HKEY_CURRENT_USER : option == 4 ? HKEY_LOCAL_MACHINE : option == 5 ? HKEY_USERS : NULL;
-    if (initialSection == NULL) return -1;
-    _tprintf(L"Input keys path (if you want to find something here, enter 'd'): ");
-    _tscanf(L"%s", subKey);
-    if (lstrcmpW(subKey, L"d") == 0) lstrcpy(subKey, L"");
-    _tprintf(L"Input search string: ");
-    _tscanf(L"%s", searchValue);
-    KeyAnalyser(initialSection, subKey, searchValue);
+    if (initialSection != NULL) {
+        _tprintf(L"Input keys path (if you want to find something here, enter 'd'): ");
+        _tscanf(L"%s", subKey);
+        if (lstrcmpW(subKey, L"d") == 0) lstrcpy(subKey, L"");
+        _tprintf(L"Input search string: ");
+        _tscanf(L"%s", searchValue);
+        KeyAnalyser(initialSection, subKey, searchValue);
+    }
 }
 
 void KeyAnalyser(HKEY initialSection, TCHAR keyPath[], LPTSTR searchValue)
@@ -210,7 +217,7 @@ void KeyAnalyser(HKEY initialSection, TCHAR keyPath[], LPTSTR searchValue)
         do
         {
             dwValueForKey = 256;
-            subkeyResultCode = RegEnumKeyEx(hKey, dwIndexForKey, subkeyName, &dwValueForKey, NULL, NULL, NULL, NULL);//выводим папки
+            subkeyResultCode = RegEnumKeyEx(hKey, dwIndexForKey, subkeyName, &dwValueForKey, NULL, NULL, NULL, NULL);
             if (subkeyResultCode == ERROR_SUCCESS)
             {
 
@@ -255,58 +262,54 @@ void KeyAnalyser(HKEY initialSection, TCHAR keyPath[], LPTSTR searchValue)
     RegCloseKey(hKey);
 }
 
-INT SaveKey()
+void SaveKey()
 {
     HKEY    hKey = 0;
     TCHAR subKey[256];
     TCHAR fileName[256];
     INT option;
-    _tprintf(L"\nSelect initial section:\n1. HKEY_CLASSES_ROOT\n2. HKEY_CURRENT_CONFIG\n3. HKEY_CURRENT_USER\n4. HKEY_LOCAL_MACHINE\n5. HKEY_USERS\nYour option: ");
+    _tprintf(L"\nSelect initial section:\n1. HKEY_CLASSES_ROOT\n2. HKEY_CURRENT_CONFIG\n\
+                3. HKEY_CURRENT_USER\n4. HKEY_LOCAL_MACHINE\n5. HKEY_USERS\nYour option: ");
     _tscanf(L"%i", &option);
     HKEY initialSection = option == 1 ? HKEY_CLASSES_ROOT : option == 2 ? HKEY_CURRENT_CONFIG : option == 3 ? HKEY_CURRENT_USER : option == 4 ? HKEY_LOCAL_MACHINE : option == 5 ? HKEY_USERS : NULL;
-    if (initialSection == NULL) return -1;
-    _tprintf(L"Input keys path (if you want to save initial section, enter 'd'): ");
-    _tscanf(L"%s", subKey);
-    if (lstrcmpW(subKey, L"d") == 0) lstrcpy(subKey, L"");
-    LONG lRes = RegOpenKeyEx(initialSection, subKey, NULL, KEY_ALL_ACCESS, &hKey);
-    if (lRes == ERROR_SUCCESS)
-    {
-        _tprintf(L"Input file name (format - path\\to\\file\\filename.file_extension): ");
-        _tscanf(L"%s", fileName);
-        DWORD errcode;
+    if (initialSection != NULL) {
+        _tprintf(L"Input keys path (if you want to save initial section, enter 'd'): ");
+        _tscanf(L"%s", subKey);
+        if (lstrcmpW(subKey, L"d") == 0) lstrcpy(subKey, L"");
+        LONG lRes = RegOpenKeyEx(initialSection, subKey, NULL, KEY_ALL_ACCESS, &hKey);
+        if (lRes == ERROR_SUCCESS)
+        {
+            _tprintf(L"Input file name (format - path\\to\\file\\filename.file_extension): ");
+            _tscanf(L"%s", fileName);
+            DWORD errcode;
 
-        LSTATUS response = RegSaveKeyEx(hKey, fileName, NULL, REG_NO_COMPRESSION);
-        errcode = GetLastError();
-        if (response == ERROR_SUCCESS)
-        {
-            _tprintf(L"Successfully!\n");
-            return 1;
+            LSTATUS response = RegSaveKeyEx(hKey, fileName, NULL, REG_NO_COMPRESSION);
+            errcode = GetLastError();
+            if (response == ERROR_SUCCESS)
+            {
+                _tprintf(L"Successfully!\n");
+            }
+            if (response == ERROR_INVALID_PARAMETER)
+            {
+                _tprintf(L"Error invalid parameter");
+            }
+            else {
+                _tprintf(L"Unsuccessfully!\n");
+                LPTSTR lpMsg = NULL;
+
+                FormatMessage(
+                    FORMAT_MESSAGE_FROM_SYSTEM |
+                    FORMAT_MESSAGE_ALLOCATE_BUFFER,
+                    NULL,
+                    response,
+                    MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+                    (LPWSTR)&lpMsg,
+                    0,
+                    NULL);
+                _tprintf(L"Error message - %s\n", lpMsg);
+                LocalFree(lpMsg);
+            }
         }
-        if (response == ERROR_INVALID_PARAMETER)
-        {
-            _tprintf(L"Error invalid parameter");
-            return -3;
-        }
-        else {
-            _tprintf(L"Unsuccessfully!\n");
-            LPTSTR lpMsg = NULL;
-            
-            FormatMessage(
-                FORMAT_MESSAGE_FROM_SYSTEM |
-                FORMAT_MESSAGE_ALLOCATE_BUFFER,
-                NULL,
-                response,
-                MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
-                (LPWSTR)&lpMsg,
-                0,
-                NULL);
-            _tprintf(L"Error message - %s\n", lpMsg);
-            LocalFree(lpMsg);
-            return -4;
-        }
+        _tprintf(L"Error opening key!\n");
     }
-    _tprintf(L"Error opening key!\n");
-        
-        return -2;
-    return -1;
 }
